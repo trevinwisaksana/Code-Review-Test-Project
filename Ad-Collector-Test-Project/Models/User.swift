c8f51cbedf0ec7f0ece634c25ab04089ad210824
Trevin Wisaksana <trevinwisaksana@gmail.com>
2018-05-23 21:08:24 +0700

    Added User Model

---
+++ Ad-Collector-Test-Project/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Ad-Collector-Test-Project
+//
+//  Created by Trevin Wisaksana on 23/05/2018.
+//  Copyright © 2018 Trevin Wisaksana. All rights reserved.
+//
+
+import Foundation
+
+final class User {
+    
+    
+    
+}
